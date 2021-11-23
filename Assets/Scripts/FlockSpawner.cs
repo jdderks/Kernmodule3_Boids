c63775002324ca76cdd16cc5c2ec200d74a9aa94
joris d <j_d_derks@hotmail.com>
2021-11-23 13:57:23 +0100

    add 2021 boids implementation

---
+++ Assets/Scripts/FlockSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FlockSpawner : MonoBehaviour
+{
+    public GameObject boidPrefab;
+
+    [Range(0, 300)]
+    public int number;
+
+    private void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Vector3 randomPositionOnScreen = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0);
+            Instantiate(boidPrefab, Camera.main.ScreenToWorldPoint(randomPositionOnScreen), Quaternion.identity);
+        }
+    }
+}
